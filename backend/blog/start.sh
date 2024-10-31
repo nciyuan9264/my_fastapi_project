20306c76943474dde700617c7f649524021564d4
WangZhenyu <wangzhenyu.9264@bytedance.com>
2024-10-31 21:00:35 +0800

    fix: 加个注释

--- backend/blog/start.sh
+++ backend/blog/start.sh
@@ -4,6 +4,7 @@ set -e
 # 启动 Celery worker
 exec poetry run celery -A blog.app.worker.celery_app worker -l info --hostname=worker1@%h -c 2 &
 exec poetry run celery -A blog.app.worker.celery_app worker -l info --hostname=worker2@%h -c 2 &
+# poetry run celery -A blog.app.worker.celery_app inspect active
 
 # 启动 Uvicorn
 exec poetry run uvicorn blog.app.main:app --host 0.0.0.0 --port 8000
